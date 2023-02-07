f29030f5f27d97384b4d54ece362ae87c74845e2
ravi <ravikumarthakur97@gmail.com>
2023-02-07 22:59:00 +0530

    Demo MVC first commit using git bash

---
+++ src/main/java/com/springmvc/config/DemoAppConfig.java
@@ -0,0 +1,16 @@
+package com.springmvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.springmvc.controller")
+public class DemoAppConfig {
+	
+	//This is config file..
+	
+	
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Welcome to Home page...</title>
+</head>
+<body>
+
+
+<h1>Welcome to Next gen solution..</h1>
+<p>We provide all the type of tech solution on cloud...</p>
+
+</body>
+</html>
